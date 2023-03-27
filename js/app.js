562a88e54d4830184c720effc3d865a19f669adf
ras1k <rasikabdullah@gmail.com>
2023-03-27 23:55:39 +0600

    api added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
-const loadPhone = async() =>{
+const loadPhone = async () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data);
+    displayPhones(data.data);
 }
 
-const displayPhones = phones =>{
-    console.log(phones)
+const displayPhones = phones => {
+    // console.log(phones);
+    const phonesContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+        // console.log(phone)
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${phone.brand}</h5>
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>`;
+        phonesContainer.appendChild(phoneDiv);
+    });
 }
 
 loadPhone()
