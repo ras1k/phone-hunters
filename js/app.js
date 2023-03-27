2f987839d88ea11833767639807af2e478aa1af7
ras1k <rasikabdullah@gmail.com>
2023-03-28 01:25:28 +0600

    content sliced

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
-const loadPhone = async () => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhone = async (searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
     displayPhones(data.data);
 }
 
 const displayPhones = phones => {
+    phones = phones.slice(0,20);
     // console.log(phones);
     const phonesContainer = document.getElementById('phone-container');
+    phonesContainer.textContent = '';
     phones.forEach(phone => {
         // console.log(phone)
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
-        <div class="card">
-            <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card p-3">
+            <img src="${phone.image}" class="card-img-top w-75 mx-auto" alt="...">
             <div class="card-body">
             <h5 class="card-title">${phone.brand}</h5>
             <h5 class="card-title">${phone.phone_name}</h5>
@@ -25,5 +27,15 @@ const displayPhones = phones => {
         phonesContainer.appendChild(phoneDiv);
     });
 }
+
+document.getElementById('btn-search').addEventListener('click', function(){
+    // if(isNaN){
+    //     return;
+    // }
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhone(searchText);
+    searchField.value = '';
+})
 
 loadPhone()
